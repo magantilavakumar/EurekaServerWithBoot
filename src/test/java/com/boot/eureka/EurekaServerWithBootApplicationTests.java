4dd71daa9060c7ed79241128be3fca9b65838806
magantilavakumar <=>
2021-04-14 22:28:40 +0800

    BaseVersion

---
+++ src/test/java/com/boot/eureka/EurekaServerWithBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerWithBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
